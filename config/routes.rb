e2a78c640cd054dc8a2150e3b883e0b3969cced0
Clement Avenel <clement.avenel@spottle.fr>
2021-12-30 00:16:34 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
       mount Sidekiq::Web => '/sidekiq'
     end
 
-
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'documentation', to:'documentation#index'
+  root to: 'home#index'
+
 end
